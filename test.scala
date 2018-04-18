5e15e7eb306c792b94bd944dfa27409147c63d6f
mortvest <dmitry.serykh@gmail.com>
2018-04-18 11:27:35 +0200

    More expression types added

--- test.scala
+++ test.scala
@@ -2,8 +2,16 @@ sealed trait Expr
 case class Attribute(attName: String) extends Expr
 case class LongConst(value: Long) extends Expr
 case class IntConst(value: Int) extends Expr
+case class StringConst(value: String) extends Expr
 case class Less(left: Expr, right: Expr) extends Expr
+case class Leq(left: Expr, right: Expr) extends Expr
+case class Greater(left: Expr, right: Expr) extends Expr
+case class Geq(left: Expr, right: Expr) extends Expr
 case class Equals(left: Expr, right: Expr) extends Expr
+case class Plus(left: Expr, right: Expr) extends Expr
+case class Minus(left: Expr, right: Expr) extends Expr
+case class And(left: Expr, right: Expr) extends Expr
+case class Or(left: Expr, right: Expr) extends Expr
 
 sealed trait RelAlg
 case class Relation(relName: String) extends RelAlg
@@ -19,10 +27,10 @@ import scala.collection.JavaConverters._
 
 object Test extends App {
   val parser = new SqlParser()
-  val query = parser.createStatement("SELECT suchka, suchka, 11 FROM zhopa WHERE a < 2")
+  val query = parser.createStatement("SELECT a, b, 11 FROM account WHERE a <= 'david' AND b <= c")
   query match {
+    // TODO: Better way for pattern matching on types
     case q if q.isInstanceOf[Query]  => println(parseSelect(q.asInstanceOf[Query]))
-    // case q if q.isInstanceOf[Query]  => println("This is a select")
     case q if q.isInstanceOf[Delete] => println("This is a delete!")
     case q if q.isInstanceOf[Insert] => println("This is an insert!")
     case _ => println("Operator is not supported")
@@ -37,10 +45,31 @@ object Test extends App {
         val compType = ex.getType.getValue
         compType match {
           case "<" => Less(left, right)
+          case "<=" => Leq(left, right)
+          case ">" => Greater(left, right)
+          case ">=" => Geq(left, right)
           // TODO: moar ops
           case _ => Equals(left, right)
         }
-      // TODO: Add parenthesis
+      case x if x.isInstanceOf[ArithmeticBinaryExpression] =>
+        val ex = x.asInstanceOf[ArithmeticBinaryExpression]
+        val left = parseExp(ex.getLeft)
+        val right = parseExp(ex.getRight)
+        val compType = ex.getType.getValue
+        compType match {
+          case "+" => Plus(left, right)
+          // TODO: moar ops
+          case _ => Minus(left, right)
+        }
+      case x if x.isInstanceOf[LogicalBinaryExpression] =>
+        val ex = x.asInstanceOf[LogicalBinaryExpression]
+        val left = parseExp(ex.getLeft)
+        val right = parseExp(ex.getRight)
+        val compType = ex.getType.toString
+        compType match {
+          case "AND" => And(left, right)
+          case "OR" => Or(left, right)
+        }
       case x if x.isInstanceOf[Identifier] =>
         val ex = x.asInstanceOf[Identifier]
         val name = ex.getValue.toString
@@ -49,32 +78,27 @@ object Test extends App {
         val ex = x.asInstanceOf[LongLiteral]
         val value = ex.getValue
         LongConst(value)
+      case x if x.isInstanceOf[StringLiteral] =>
+        val ex = x.asInstanceOf[StringLiteral]
+        val value = ex.getValue
+        StringConst(value)
     }
   }
 
   def parseSelect(query: Query) = {
     def transSelect (lst: List[SelectItem]): List[Expr] = {
-      // TODO: Use expression evaluation here
       lst match {
-        case x::xs if x.asInstanceOf[SingleColumn].getExpression.
-            isInstanceOf[LongLiteral] =>
-          LongConst(x.asInstanceOf[SingleColumn].getExpression.
-            asInstanceOf[LongLiteral].getValue)::transSelect(xs)
-        case x::xs if x.asInstanceOf[SingleColumn].getExpression.
-            isInstanceOf[Identifier] =>
-          Attribute(x.asInstanceOf[SingleColumn].getExpression.
-            asInstanceOf[Identifier].getValue.toString)::transSelect(xs)
+        case x::xs if x.asInstanceOf[SingleColumn].getExpression.isInstanceOf[LongLiteral] =>
+          parseExp(x.asInstanceOf[SingleColumn].getExpression) :: transSelect(xs)
+        case x::xs if x.asInstanceOf[SingleColumn].getExpression.isInstanceOf[Identifier] =>
+          parseExp(x.asInstanceOf[SingleColumn].getExpression) :: transSelect(xs)
         case Nil => Nil
       }
     }
-
     val body = query.getQueryBody.asInstanceOf[QuerySpecification]
     val select = body.getSelect
     val where = toScala(body.getWhere)
-    println(where)
     val from = toScala(body.getFrom).get.asInstanceOf[Table].getName.toString
-    println(from)
-    // val from = "default name"
     val columns = select.getSelectItems.asScala.toList
       (select, where) match {
       case (select, Some(whr)) =>
