4f1aa8a2708b0780a0c95f64239e067628475f5e
mortvest <dmitry.serykh@gmail.com>
2018-06-01 21:19:24 +0200

    Namespace management added

--- ddlProcessor.scala
+++ ddlProcessor.scala
@@ -1,26 +1,38 @@
 import CodeGenFunctions._
 
 object DDLProcessor {
-  def apply(meta: Map[String, TableMetaData], schemaFileName: String, typeDefFileName: String ) = {
-    val schemaCreation = genSchema(meta, "jaqco", typeDefFileName)
-    val typeCreation = genTypeDef(meta)
+  def apply(
+    meta: Map[String, TableMetaData],
+    schemaFileName: String,
+    typeDefFileName: String,
+    namespace: String
+  ) = {
+    val schemaCreation = genSchema(meta, "jaqco", typeDefFileName, namespace)
+    val typeCreation = genTypeDef(meta, namespace)
     (schemaCreation, typeCreation)
   }
 
-  def genSchema(metaMap: Map[String, TableMetaData], schemaName: String, typeDefFileName: String) = {
-    def genIndex(indexName: String, typeName: String): String = {
-      s"""my_database->create_index("$indexName", sizeof($typeName), false);"""
-    }
-    s"""#include "$typeDefFileName"
-class ${schemaName}_schema_creator: public reactdb::abstract_schema_creator {
-public:
-    void create_schema(reactdb::hypo_db* my_database) override {
+  def genSchema(
+    metaMap: Map[String, TableMetaData],
+    schemaName: String,
+    typeDefFileName: String,
+    namespace: String
+  ) = {
+      def genIndex(indexName: String, typeName: String): String = {
+        s"""my_database->create_index("$indexName", sizeof($typeName), false);"""
+      }
+      s"""#include "$typeDefFileName"
+namespace $namespace {
+  class ${schemaName}_schema_creator: public reactdb::abstract_schema_creator {
+  public:
+      void create_schema(reactdb::hypo_db* my_database) override {
 ${metaMap.foldLeft("")( (acc, x) => acc + "        " + genIndex(x._1, x._1 + "_key_type") + "\n" )}
-    }
-}
-;"""
+      }
   }
-  def genTypeDef(metaMap: Map[String, TableMetaData]): String = {
+  ;
+}"""
+  }
+  def genTypeDef(metaMap: Map[String, TableMetaData], namespace: String): String = {
     def proc(meta: TableMetaData) = {
       val attributes = meta.attributes
       val indexParts = meta.indexParts
@@ -29,6 +41,9 @@ ${metaMap.foldLeft("")( (acc, x) => acc + "        " + genIndex(x._1, x._1 + "_k
       val valStruct = newValueStruct(meta, name+"_val_type")
       keyStruct + "\n" + valStruct
     }
-    metaMap.foldLeft("")( (acc, x) => acc + proc(x._2) + "\n" ).dropRight(1)
+    s"""namespace $namespace {
+metaMap.foldLeft("")( (acc, x) => acc + proc(x._2) + "\n" ).dropRight(1)
+}"""
   }
 }
+
--- main.scala
+++ main.scala
@@ -14,12 +14,12 @@ case class SimpleRef(counter: Int, fun: (Int => Unit))
 // Setting up command line arguments
 class Conf(arguments: Seq[String]) extends ScallopConf(arguments) {
   val codeFiles = opt[List[String]](descr = "List of code files with embedded SQL(.jsql)")
-  val ddlFiles = opt[List[String]](descr = "List of DDL files(.ddl)")
+  val ddlFile = opt[String](descr = "DDL file(.ddl), name of the files will be the name of the namespace")
   val outDir = opt[String](descr = "Destination folder", short = 'o')
   val folder = opt[String](descr = "Folder with .ddl and .jsql files")
   val debug = toggle("debug", short = 'g')
   requireOne(codeFiles, folder)
-  codependent(codeFiles, ddlFiles)
+  codependent(codeFiles, ddlFile)
   verify()
 }
 
@@ -37,13 +37,12 @@ object Main{
       case (None, Some(dir)) => s"$dir/$defaultDir"
       case _ => ""
     }
-    (conf.codeFiles.toOption, conf.ddlFiles.toOption, conf.folder.toOption) match {
-      case (Some(codeFiles), Some(ddlFiles), None) =>
-        checkDuplicates(ddlFiles, "File")
+    (conf.codeFiles.toOption, conf.ddlFile.toOption, conf.folder.toOption) match {
+      case (Some(codeFiles), Some(ddlFile), None) =>
         checkDuplicates(codeFiles, "File")
         for (fileName <- codeFiles) {checkExt(fileName, codeExt)}
-        for (fileName <- ddlFiles) {checkExt(fileName, ddlExt)}
-        processFiles(ddlFiles, codeFiles, outDir)
+        checkExt(ddlFile, ddlExt)
+        processFiles(ddlFile, codeFiles, outDir)
       case (None, None, Some(folder)) =>
         // println(s"folder is: ${folder}")
         val dirList = ls(File(folder)).toList.map {case x => x.toString}
@@ -51,27 +50,29 @@ object Main{
         if (codeFiles.size < 1)
           throw new Error(s"""No code files(.jsql) were found in the folder "${folder}"""")
         val ddlFiles = extFilter(dirList, ddlExt)
-        if (codeFiles.size < 1)
-          throw new Error(s"""No DDL files(.ddl) were found in the folder "${folder}"""")
-        processFiles(ddlFiles, codeFiles, outDir)
+        if (ddlFiles.size < 1)
+          throw new Error(s"""DDL file(.ddl) was not found in the folder "${folder}"""")
+        else if (ddlFiles.size > 1)
+          throw new Error(s"""Multiple DDL files (.ddl) were not found in the folder "${folder}"""")
+        processFiles(ddlFiles.head, codeFiles, outDir)
       case _ => throw new Error(s"Command line argument processing failed")
     }
   }
-  def processFiles(ddlFiles: List[String], codeFiles: List[String], outDir: String) = {
+  def processFiles(ddlFile: String, codeFiles: List[String], outDir: String) = {
     // println(s"Code files: ${codeFiles}")
-    // println(s"DDL files: ${ddlFiles}")
+    // println(s"DDL file: ${ddlFile}")
     File(outDir).createIfNotExists(true).clear()
-    val ddlCode = ddlFiles.map { case x => File(x).contentAsString }
-    val ddlList =
-      ddlCode.foldLeft(List[String]()) ((acc, x) => Preprocessor.processDDL(x) ::: acc)
+    val ddlCode = File(ddlFile).contentAsString
+    val ddlList = Preprocessor.processDDL(ddlCode)
     val st = ddlList.map{ case x => createMeta(x) }
     st.groupBy(_._1).collect { case (x,ys) if ys.lengthCompare(1) > 0 => x } match {
       case Nil => {}
       case x::xs => throw new Error(s"Table ${x} is created twice")
     }
     val meta = st.toMap
+    val namespace = findFileName(ddlFile, ddlExt)
     val (scheCrea, typeDef) =
-      DDLProcessor(meta, s"schema_creator_$filePostfix.h", s"type_definition_$filePostfix.h")
+      DDLProcessor(meta, s"schema_creator_$filePostfix.h", s"type_definition_$filePostfix.h", namespace)
     createFile(outDir, "jaqco_schema_creator.h", scheCrea)
     createFile(outDir, "jaqco.h", typeDef)
     processCodeFiles(codeFiles, meta, outDir)
@@ -100,6 +101,7 @@ object Main{
   def extFilter(dirList: List[String], ext: String) = {
     dirList.filter{ case x => (s".+${ext}"r).findFirstIn(x).isDefined }
   }
+
   def createMeta(sql: String) = {
     val parser = new SqlParser()
     val query = parser.createStatement(sql)
